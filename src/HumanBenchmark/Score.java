6854ea3365df6c28e19448727f53867f3b91c39c
Jacob Varela <varelajacob@unm.edu>
2020-10-29 19:27:45 -0600

    Created game class with constructor.

---
+++ src/HumanBenchmark/Score.java
@@ -0,0 +1,20 @@
+package HumanBenchmark;
+
+public class Score {
+    
+    private int reactionScore, aimScore, chimpscore, visualScore;
+    private int customScore, typingScore, numMemScore, verbMemScore;
+
+    public void initializeScores(){
+        this.reactionScore = 0;
+        this.aimScore = 0;
+        this.chimpscore = 0;
+        this.visualScore = 0;
+        this.customScore = 0;
+        this.typingScore = 0;
+        this.numMemScore = 0;
+        this.verbMemScore = 0;
+    }
+
+    
+}
