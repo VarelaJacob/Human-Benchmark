ada757e8850f28016fb9fa4df47ab684772db8de
Jacob Varela <varelajacob@unm.edu>
2020-10-29 18:04:21 -0600

    Updated BorderPane to look like the website

--- src/HumanBenchmark/MainGameLoop.java
+++ src/HumanBenchmark/MainGameLoop.java
@@ -1,10 +1,29 @@
 package HumanBenchmark;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+
+/**
+ *************** To Do ***************
+ * Add home button event handler
+ * Add get started button event handler
+ */
 
 public class MainGameLoop extends Application {
 
@@ -17,16 +36,69 @@ public class MainGameLoop extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+
         primaryStage.setTitle("Human Benchmark - Jacob Varela");
-        homeButton = new Button("Go home");
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(homeButton);
-
-        Scene scene = new Scene(layout, 1600, 900);
+        BorderPane humanPane = createBorderPane();
+        Scene scene = new Scene(humanPane, 1600, 900);
         primaryStage.setScene(scene);
         primaryStage.show();
 
     }
+
+    private BorderPane createBorderPane() {
+
+        BorderPane border = new BorderPane();
+
+        HBox hboxTop = new HBox();
+        hboxTop.setStyle("-fx-background-color: #e6e8f4;");
+        hboxTop.setPadding(new Insets(0, 0, 0, 20));
+        hboxTop.setSpacing(20);
+        
+        ImageView iconView = new ImageView( new Image("file:resources/humanBenchmarkicon.png"));
+
+        Label mainLabel = new Label("Human Benchmark");
+        mainLabel.setFont(Font.font("Arial", FontWeight.BOLD,70));
+        mainLabel.setTextFill(Color.web("#FFFFFF"));
+
+        Label subLabel = new Label("Measure your abilities with brain games and cognitive tests.");
+        subLabel.setFont(Font.font("Arial", 20));
+        subLabel.setTextFill(Color.web("#FFFFFF"));
+
+        Button getStartedBtn = new Button("Get Started");
+        getStartedBtn.setStyle("-fx-background-color: #ffd154");
+        getStartedBtn.setPrefSize(145, 45);
+        getStartedBtn.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        
+        VBox vboxDefault = new VBox();
+        vboxDefault.setPadding(new Insets(10, 10, 10, 10));
+        vboxDefault.setStyle("-fx-background-color: #2b86d1");
+        vboxDefault.setMaxHeight(550);
+        vboxDefault.setSpacing(30);
+        vboxDefault.getChildren().addAll(iconView,mainLabel,subLabel,getStartedBtn);
+
+        vboxDefault.setAlignment(Pos.CENTER);
+        
+        
+
+        homeButton = new Button("Human Benchmark (home)");
+        homeButton.setMinHeight(35);
+        homeButton.setStyle("-fx-background-color: #f2f2f2");
+        homeButton.setStyle("-fx-border-color: #000000");
+        hboxTop.getChildren().addAll(homeButton);/*
+        homeButton.setOnAction(new EventHandler<ActionEvent>(){
+            
+            @Override
+            public void handle(ActionEvent event) {
+                
+            }
+        });*/
+
+        border.setTop(hboxTop);
+        border.setCenter(vboxDefault);
+
+        return border;
+    }
+
+    
 }
