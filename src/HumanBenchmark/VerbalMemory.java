da64687b89cae4d305126a3503c8ff6bc6a91d57
Jacob Varela <varelajacob@unm.edu>
2020-10-30 17:31:58 -0600

    Fixed game logic issues

--- src/HumanBenchmark/VerbalMemory.java
+++ src/HumanBenchmark/VerbalMemory.java
@@ -27,35 +27,34 @@ public class VerbalMemory {
     
     String BACKGROUNDBLUE   = "-fx-background-color: #2b86d1";
     String BACKGROUNDYELLOW = "-fx-background-color: #ffd154";
-    private int highScore;
-    private boolean gameInProgress;
+    private int highScore, lives, randIndex, currentScore;
     private ArrayList<String> wordBank = new ArrayList<>();
+    private HBox statBox, gameBtnsBox;
+    private VBox vboxDefault;
+    private Label wordLabel, livesLabel, currScoreLabel, mainLabel;
+    private Random rand = new Random();
+    private ImageView iconView = new ImageView(new Image("file:resources/verbalIcon2.png"));
+    private Button startTestBtn;
+    private boolean[] seenArray;
 
     public VBox playGame() {
 
-        //File wordFile = new File("file:resources/wordBank.txt");
-        ImageView iconView = new ImageView(new Image("file:resources/verbalIcon2.png"));
-
         populateWordBank();
 
-        String currWord;
-        int lives, currentScore, randIndex;
-        boolean[] seenArray = new boolean[100];
+        startGame();
 
-        Random rand = new Random();
+        return vboxDefault;
+    }
+
+    private void startGame() {
+        seenArray = new boolean[100];
         randIndex = rand.nextInt(100);
-
-        currWord = wordBank.get(randIndex);
-        seenArray[randIndex] = true;
-
-        lives = 3;
-        currentScore = 0;        
-        gameInProgress = false;
         Arrays.fill(seenArray, false);
-
-
-
-        Label mainLabel = new Label("Verbal Memory Test");
+        
+        this.lives = 3;
+        this.currentScore = 0;        
+        
+        mainLabel = new Label("Verbal Memory Test");
         mainLabel.setFont(Font.font("Arial", FontWeight.BOLD, 50));
         mainLabel.setTextFill(Color.web("#FFFFFF"));
 
@@ -75,15 +74,15 @@ public class VerbalMemory {
         scoreLabel.setFont(Font.font("Arial", FontPosture.ITALIC, 14));
         scoreLabel.setTextFill(Color.web("#FFFFFF")); 
         
-        Label livesLabel = new Label("Lives | " + String.valueOf(lives));
+        livesLabel = new Label("Lives | " + String.valueOf(lives));
         livesLabel.setFont(Font.font("Arial", 20));
         livesLabel.setTextFill(Color.web("#FFFFFF")); 
 
-        Label currScoreLabel = new Label("Score | " + String.valueOf(currentScore));
+        currScoreLabel = new Label("Score | " + String.valueOf(currentScore));
         currScoreLabel.setFont(Font.font("Arial", 20));
         currScoreLabel.setTextFill(Color.web("#FFFFFF")); 
 
-        Label wordLabel = new Label(currWord);
+        wordLabel = new Label("Current word : " + wordBank.get(randIndex));
         wordLabel.setFont(Font.font("Arial", 40));
         wordLabel.setTextFill(Color.web("#FFFFFF")); 
 
@@ -91,24 +90,18 @@ public class VerbalMemory {
         seenBtn.setStyle(BACKGROUNDYELLOW);
         seenBtn.setPrefSize(145, 45);
         seenBtn.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        seenBtn.setOnMouseClicked( ( e ) -> {
-            /* play*/
-        });
-
+        
         Button newBtn = new Button("NEW");
         newBtn.setStyle(BACKGROUNDYELLOW);
         newBtn.setPrefSize(145, 45);
         newBtn.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        newBtn.setOnMouseClicked( ( e ) -> {
-            /* play*/
-        });
 
-        Button startTestBtn = new Button("Start");
+        startTestBtn = new Button("Start");
         startTestBtn.setStyle(BACKGROUNDYELLOW);
         startTestBtn.setPrefSize(145, 45);
         startTestBtn.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-        VBox vboxDefault = new VBox();
+        vboxDefault = new VBox();
         vboxDefault.setPadding(new Insets(10, 10, 10, 10));
         vboxDefault.setStyle(BACKGROUNDBLUE);
         vboxDefault.setMinHeight(550);
@@ -119,37 +112,92 @@ public class VerbalMemory {
 
         startTestBtn.setOnMouseClicked( ( e ) -> {
 
-            gameInProgress = true;
+            this.lives = 3;
+            this.currentScore = 0;
+            seenArray = new boolean[100];
+            randIndex = rand.nextInt(100);
+            Arrays.fill(seenArray, false);
 
-            HBox statBox = new HBox();
+            statBox = new HBox();
             statBox.setAlignment(Pos.CENTER);
+            livesLabel.setText("Lives | " + String.valueOf(lives));
+            currScoreLabel.setText("Score | " + String.valueOf(currentScore));
             statBox.getChildren().addAll(livesLabel, currScoreLabel);
             statBox.setSpacing(30);
 
-            HBox gameBtnsBox = new HBox();
+            gameBtnsBox = new HBox();
             gameBtnsBox.setAlignment(Pos.CENTER);
             gameBtnsBox.getChildren().addAll(seenBtn, newBtn);
             gameBtnsBox.setSpacing(30);
 
             vboxDefault.getChildren().clear();
-            vboxDefault.getChildren().addAll(statBox, gameBtnsBox);
+            vboxDefault.getChildren().addAll(statBox, wordLabel, gameBtnsBox);
         });
 
-        return vboxDefault;
+        newBtn.setOnMouseClicked( ( e ) -> {
+            if( !isRepeatWord(seenArray, randIndex) ) {
+                seenArray[randIndex] = true;                
+                this.currentScore = (currentScore +1);
+                update(currentScore, lives);
+            }
+            else{
+                this.lives = (lives - 1);
+                update(currentScore, lives);
+            }
+        });
+
+        seenBtn.setOnMouseClicked( ( e ) -> {
+            if( isRepeatWord(seenArray, randIndex) ) {     
+                this.currentScore = (currentScore +1);
+                update(currentScore, lives);
+            }
+            else{
+                this.lives = (lives - 1);
+                update(currentScore, lives);
+            }
+        });
     }
 
-    /*
-    private void updateScore() {
+    private void update(int score, int lives) {
+
         if(highScore == 0 ){
-            this.highScore = scoreTime;
+            this.highScore = score; 
         }
-        else if(elapsedTime < highScore){
-            this.highScore = scoreTime;
+        else if(score > highScore){
+            this.highScore = score;
+        }
+
+        if( lives == 0){
+            vboxDefault.getChildren().clear();
+            mainLabel.setText("Verbal Memory score:");
+            currScoreLabel.setText(String.valueOf(currentScore) + " word(s)");
+            startTestBtn.setText("Try Again");
+            
+            vboxDefault.getChildren().addAll(iconView, mainLabel, currScoreLabel, startTestBtn);
         }
         else {
-            //do nothing.
+            this.currentScore = score;
+            
+            this.lives = lives;
+            randIndex = rand.nextInt(100);
+            wordLabel.setText("Current word : " + wordBank.get(randIndex));
+            livesLabel.setText("Lives | " + String.valueOf(lives));
+            currScoreLabel.setText("Score | " + String.valueOf(currentScore));
+            statBox.getChildren().clear();
+            statBox.getChildren().addAll(livesLabel, currScoreLabel);
+            vboxDefault.getChildren().clear();
+            vboxDefault.getChildren().addAll(statBox, wordLabel, gameBtnsBox);
         }
-    }*/
+    }
+
+
+    private boolean isRepeatWord(boolean[] seenArray, int randIndex) {
+        
+        if( seenArray[randIndex] == true ){
+            return true;
+        }        
+        return false;
+    }
 
     public ArrayList<String> populateWordBank() {
         
@@ -159,8 +207,6 @@ public class VerbalMemory {
         try{ 
             reader = new BufferedReader(
                 new FileReader(fileName, StandardCharsets.UTF_8));
-
-            var sb = new StringBuilder();
 
             String nextWord;
             while ((nextWord = reader.readLine()) != null) {
