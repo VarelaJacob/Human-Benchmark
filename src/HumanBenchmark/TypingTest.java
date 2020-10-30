ab461cb482bd3bd56518928cfa4cadc99abd0293
Jacob Varela <varelajacob@unm.edu>
2020-10-30 12:53:57 -0600

    Added TypingTest homepage Vbox and gameScreen

---
+++ src/HumanBenchmark/TypingTest.java
@@ -0,0 +1,93 @@
+package HumanBenchmark;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.paint.Color;
+
+/**
+ * 
+ */
+public class TypingTest {
+    
+    String BACKGROUNDBLUE = "-fx-background-color: #2b86d1";
+    private int highScore;
+    private boolean gameInProgress;
+
+    public VBox playGame() {
+
+        Label mainLabel = new Label("Typing Test");
+        mainLabel.setFont(Font.font("Arial", FontWeight.BOLD, 50));
+        mainLabel.setTextFill(Color.web("#FFFFFF"));
+
+        Label subLabel1 = new Label("How many words per minute");
+        subLabel1.setFont(Font.font("Arial", 20));
+        subLabel1.setTextFill(Color.web("#FFFFFF"));
+
+        Label scoreLabel = new Label("HighScore: " + String.valueOf(highScore));
+        scoreLabel.setFont(Font.font("Arial", FontPosture.ITALIC, 14));
+        scoreLabel.setTextFill(Color.web("#FFFFFF"));      
+
+        VBox vboxDefault = new VBox();
+        vboxDefault.setPadding(new Insets(10, 10, 10, 10));
+        vboxDefault.setStyle(BACKGROUNDBLUE);
+        vboxDefault.setMinHeight(550);
+        vboxDefault.setMaxHeight(550);
+        vboxDefault.setSpacing(20);
+        vboxDefault.getChildren().addAll(scoreLabel, mainLabel, subLabel1);
+        vboxDefault.setAlignment(Pos.CENTER);
+
+        return vboxDefault;
+    }
+
+    /*
+    private void updateScore() {
+        if(highScore == 0 ){
+            this.highScore = scoreTime;
+        }
+        else if(elapsedTime < highScore){
+            this.highScore = scoreTime;
+        }
+        else {
+            //do nothing.
+        }
+    }*/
+
+    public int getHighScore(){
+        return highScore;
+    }
+
+    public VBox createVBox() {
+
+        this.highScore = 0;
+
+        ImageView iconView = new ImageView( new Image("file:resources/typingIcon.png"));
+
+        Label mainLabel = new Label("Typing");
+        mainLabel.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+
+        Label subLabel = new Label("How many words per minute");
+        subLabel.setFont(Font.font("Arial", 12));
+
+        Label subLabel2 = new Label("can you type?");
+        subLabel.setFont(Font.font("Arial", 12));
+
+        VBox vbox = new VBox();
+        vbox.setPrefSize(250, 250);
+        vbox.setStyle("-fx-background-color: #FFFFFF");
+        vbox.setPadding(new Insets(10, 10, 10, 10));
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(20);
+
+        vbox.getChildren().addAll(iconView, mainLabel, subLabel, subLabel2);
+
+        return vbox;
+    }
+}
