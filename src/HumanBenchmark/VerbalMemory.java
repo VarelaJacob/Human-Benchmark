afb3eed2a5777595d719eada2159507c4323817a
Jacob Varela <varelajacob@unm.edu>
2020-10-30 16:33:04 -0600

    Read in from wordbank text file

--- src/HumanBenchmark/VerbalMemory.java
+++ src/HumanBenchmark/VerbalMemory.java
@@ -1,11 +1,19 @@
 package HumanBenchmark;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -17,13 +25,34 @@ import javafx.scene.paint.Color;
  */
 public class VerbalMemory {
     
-    String BACKGROUNDBLUE = "-fx-background-color: #2b86d1";
+    String BACKGROUNDBLUE   = "-fx-background-color: #2b86d1";
+    String BACKGROUNDYELLOW = "-fx-background-color: #ffd154";
     private int highScore;
     private boolean gameInProgress;
+    private ArrayList<String> wordBank = new ArrayList<>();
 
     public VBox playGame() {
 
+        //File wordFile = new File("file:resources/wordBank.txt");
         ImageView iconView = new ImageView(new Image("file:resources/verbalIcon2.png"));
+
+        populateWordBank();
+
+        String currWord;
+        int lives, currentScore, randIndex;
+        boolean[] seenArray = new boolean[100];
+
+        Random rand = new Random();
+        randIndex = rand.nextInt(100);
+
+        currWord = wordBank.get(randIndex);
+        seenArray[randIndex] = true;
+
+        lives = 3;
+        currentScore = 0;        
+        gameInProgress = false;
+        Arrays.fill(seenArray, false);
+
 
 
         Label mainLabel = new Label("Verbal Memory Test");
@@ -44,15 +73,40 @@ public class VerbalMemory {
 
         Label scoreLabel = new Label("HighScore: " + String.valueOf(highScore) + " word(s)");
         scoreLabel.setFont(Font.font("Arial", FontPosture.ITALIC, 14));
-        scoreLabel.setTextFill(Color.web("#FFFFFF"));      
+        scoreLabel.setTextFill(Color.web("#FFFFFF")); 
+        
+        Label livesLabel = new Label("Lives | " + String.valueOf(lives));
+        livesLabel.setFont(Font.font("Arial", 20));
+        livesLabel.setTextFill(Color.web("#FFFFFF")); 
 
-        Button startTestBtn = new Button("Start");
-        startTestBtn.setStyle("-fx-background-color: #ffd154");
-        startTestBtn.setPrefSize(145, 45);
-        startTestBtn.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        startTestBtn.setOnMouseClicked( ( e ) -> {
+        Label currScoreLabel = new Label("Score | " + String.valueOf(currentScore));
+        currScoreLabel.setFont(Font.font("Arial", 20));
+        currScoreLabel.setTextFill(Color.web("#FFFFFF")); 
+
+        Label wordLabel = new Label(currWord);
+        wordLabel.setFont(Font.font("Arial", 40));
+        wordLabel.setTextFill(Color.web("#FFFFFF")); 
+
+        Button seenBtn = new Button("SEEN");
+        seenBtn.setStyle(BACKGROUNDYELLOW);
+        seenBtn.setPrefSize(145, 45);
+        seenBtn.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        seenBtn.setOnMouseClicked( ( e ) -> {
             /* play*/
         });
+
+        Button newBtn = new Button("NEW");
+        newBtn.setStyle(BACKGROUNDYELLOW);
+        newBtn.setPrefSize(145, 45);
+        newBtn.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        newBtn.setOnMouseClicked( ( e ) -> {
+            /* play*/
+        });
+
+        Button startTestBtn = new Button("Start");
+        startTestBtn.setStyle(BACKGROUNDYELLOW);
+        startTestBtn.setPrefSize(145, 45);
+        startTestBtn.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
         VBox vboxDefault = new VBox();
         vboxDefault.setPadding(new Insets(10, 10, 10, 10));
@@ -62,6 +116,24 @@ public class VerbalMemory {
         vboxDefault.setSpacing(20);
         vboxDefault.getChildren().addAll(scoreLabel, iconView,mainLabel, subLabel1, subLabel2, startTestBtn);
         vboxDefault.setAlignment(Pos.CENTER);
+
+        startTestBtn.setOnMouseClicked( ( e ) -> {
+
+            gameInProgress = true;
+
+            HBox statBox = new HBox();
+            statBox.setAlignment(Pos.CENTER);
+            statBox.getChildren().addAll(livesLabel, currScoreLabel);
+            statBox.setSpacing(30);
+
+            HBox gameBtnsBox = new HBox();
+            gameBtnsBox.setAlignment(Pos.CENTER);
+            gameBtnsBox.getChildren().addAll(seenBtn, newBtn);
+            gameBtnsBox.setSpacing(30);
+
+            vboxDefault.getChildren().clear();
+            vboxDefault.getChildren().addAll(statBox, gameBtnsBox);
+        });
 
         return vboxDefault;
     }
@@ -79,7 +151,30 @@ public class VerbalMemory {
         }
     }*/
 
-    public int getHighScore(){
+    public ArrayList<String> populateWordBank() {
+        
+        String fileName = "resources/workBank.txt";
+
+        BufferedReader reader = null;
+        try{ 
+            reader = new BufferedReader(
+                new FileReader(fileName, StandardCharsets.UTF_8));
+
+            var sb = new StringBuilder();
+
+            String nextWord;
+            while ((nextWord = reader.readLine()) != null) {
+                wordBank.add(nextWord);
+            }
+        
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return wordBank;
+    }
+
+    public int getHighScore() {
         return highScore;
     }
 
