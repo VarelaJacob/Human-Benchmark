60d5ce250f40aa882b44f42910e3d84c2f6b9d0c
Jacob Varela <varelajacob@unm.edu>
2021-01-20 15:16:28 -0700

    Create new class to store high scores in a database

---
+++ src/HumanBenchmark/HighScores.java
@@ -0,0 +1,13 @@
+package HumanBenchmark;
+
+import java.sql.Connection;
+
+/**
+ * 
+ */
+public class HighScores {
+
+    public static Connection con;
+    public static boolean hasData = false;
+    
+}
