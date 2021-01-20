7a8910b7472db2d1feb2c680453c590e268f618f
Jacob Varela <varelajacob@unm.edu>
2021-01-20 15:49:46 -0700

    Initialize all High Scores in the game TABLE to 0.

--- src/HumanBenchmark/HighScores.java
+++ src/HumanBenchmark/HighScores.java
@@ -2,6 +2,7 @@ package HumanBenchmark;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -73,10 +74,50 @@ public class HighScores {
                
                 // Build the table
                 Statement state2 = connection.createStatement();
-                state2.execute("CREATE TABLE game(gameName varchar(10), "+
-                " score long, primary key(gameName));");
+                state2.execute("CREATE TABLE game(" 
+                + "name varchar(15), highScore long, primary key(name));");
 
-                
+                // Initialize AimTrainer High Score to zero.
+                PreparedStatement prep = connection.prepareStatement(
+                    "INSERT INTO game values(?, ?);");
+                prep.setString(1, "AimTrainer");
+                prep.setLong  (2, 0);
+                prep.execute();
+
+                // Initialize ChimpTest High Score to zero.
+                PreparedStatement prep2 = connection.prepareStatement(
+                    "INSERT INTO game values(?, ?);");
+                prep2.setString(1, "ChimpTest");
+                prep2.setLong  (2, 0);
+                prep2.execute();
+
+                // Initialize NumberMemory High Score to zero.
+                PreparedStatement prep3 = connection.prepareStatement(
+                    "INSERT INTO game values(?, ?);");
+                prep3.setString(1, "NumberMemory");
+                prep3.setLong  (2, 0);
+                prep3.execute();
+
+                // Initialize ReactionTest High Score to zero.
+                PreparedStatement prep4 = connection.prepareStatement(
+                    "INSERT INTO game values(?, ?);");
+                prep4.setString(1, "ReactionTest");
+                prep4.setLong  (2, 0);
+                prep4.execute();
+
+                // Initialize VerbalMemory High Score to zero.
+                PreparedStatement prep5 = connection.prepareStatement(
+                    "INSERT INTO game values(?, ?);");
+                prep5.setString(1, "VerbalMemory");
+                prep5.setLong  (2, 0);
+                prep5.execute();
+
+                // Initialize VisualMemory High Score to zero.
+                PreparedStatement prep6 = connection.prepareStatement(
+                    "INSERT INTO game values(?, ?);");
+                prep6.setString(1, "VisualMemory");
+                prep6.setLong  (2, 0);
+                prep6.execute();               
             }
         }
     }
