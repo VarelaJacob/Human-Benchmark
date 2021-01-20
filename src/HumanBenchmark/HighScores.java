862ddb504533afe8377ffbffdccc62841092daee
Jacob Varela <varelajacob@unm.edu>
2021-01-20 15:22:11 -0700

    Create method to return a query of high scores from each game.

--- src/HumanBenchmark/HighScores.java
+++ src/HumanBenchmark/HighScores.java
@@ -1,13 +1,33 @@
 package HumanBenchmark;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * 
  */
 public class HighScores {
 
-    public static Connection con;
+    // Handles connection to the db.
+    public static Connection connection;
     public static boolean hasData = false;
     
+    /**
+     * This method will display the currently recorded high scores for
+     * each of the games.
+     * @return a ResultSet containing the high scores from each game.
+     */
+    public ResultSet displayScores() {
+
+        if(connection == null){
+            getConnection();
+        }
+
+        Statement state = connection.createStatement();
+
+        ResultSet scores = state.executeQuery("SELECT score FROM game");
+
+        return scores;
+    }
 }
